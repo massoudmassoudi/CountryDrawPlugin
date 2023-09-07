bf9f1d84998005844e9f626ee8984da50c402b2b
Massoud Massoudi <massoud.massoudi@hotmail.com>
2023-09-07 07:49:05 +0530

    Added Drawing shapes features

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import "./styles.css";
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { FeatureGroup, MapContainer, TileLayer } from "react-leaflet";
+import {EditControl} from "react-leaflet-draw"
+import "leaflet-draw/dist/leaflet.draw.css"
+
 
 
 export default function App() {
   return (
-    <MapContainer center={[20.5937, 78.9629]} zoom={5}>
+    <MapContainer center={[21.5937, 80.9629]} zoom={5}>
       {/* OPEN STREET MAPS TILES */}
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <FeatureGroup>
+        <EditControl  position="bottomright"/>
+      </FeatureGroup>
 
     </MapContainer>
   );
